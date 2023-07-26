a37feead7c7d93e73ed93d97db97e0a638817ca6
Pavle Djurdjic <Pavle Djurdjic@MANGORO>
2023-07-26 19:43:39 +0200

    Slight refactor

--- NeetCode/Two_sum.h
+++ NeetCode/Two_sum.h
@@ -1,17 +1,16 @@
 #pragma once
-#include <unordered_map>
 #include <map>
 using namespace std;
 
 int* two_sum(int A[], int n, int target, int result[]) {
-	map<int, int> umap;
+	map<int, int> map;
 
 	for (int i = 0; i < n; i++) {
-		if (umap.find(A[i]) != umap.end()) {
+		if (map.find(A[i]) != map.end()) {
 			result[0] = A[i];
 			result[1] = target - A[i];
 			return result;
 		}
-		umap[target - A[i]] = 1;
+		map[target - A[i]] = 1;
 	}
 }
