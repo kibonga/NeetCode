845de55f19b46b3eeeee22df9128e8aa70599cd2
Pavle Djurdjic <Pavle Djurdjic@MANGORO>
2023-08-31 16:11:28 +0200

    Working solution for reorder linked list using stack

--- NeetCode/NeetCode.cpp
+++ NeetCode/NeetCode.cpp
@@ -2,10 +2,25 @@
 //
 
 #include <iostream>
+#include "Reorder_list.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	ListNode* node1 = new ListNode(1);
+	ListNode* node2 = new ListNode(2);
+	ListNode* node3 = new ListNode(3);
+	ListNode* node4 = new ListNode(4);
+	ListNode* node5 = new ListNode(5);
+	ListNode* node6 = new ListNode(6);
+	
+	node1->next = node2;
+	node2->next = node3;
+	node3->next = node4;
+	node4->next = node5;
+	node5->next = node6;
+
+	ListNode* result = reorder(node1);
+	display(result);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
