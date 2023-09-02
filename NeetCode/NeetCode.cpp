47438674cf5d411bc4235a5b78c73523334388f9
Pavle Djurdjic <pavledjurdjic30@gmail.com>
2023-09-02 08:37:46 +0200

    Slight code refactor

--- NeetCode/NeetCode.cpp
+++ NeetCode/NeetCode.cpp
@@ -1,36 +1,38 @@
-// NeetCode.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// NeetCode.cpp : This file contains the 'main' function. Program execution
+// begins and ends there.
 //
 
-#include <iostream>
 #include "Reorder_list.h"
+#include <iostream>
 
-int main()
-{
-    LinkedNode* node1 = new LinkedNode(1);
-    LinkedNode* node2 = new LinkedNode(2);
-    LinkedNode* node3 = new LinkedNode(3);
-    LinkedNode* node4 = new LinkedNode(4);
-    LinkedNode* node5 = new LinkedNode(5);
-    LinkedNode* node6 = new LinkedNode(6);
-    
-    node1->next = node2;
-    node2->next = node3;
-    node3->next = node4;
-    node4->next = node5;
-    node5->next = node6;
+int main() {
+  LinkedNode *node1 = new LinkedNode(1);
+  LinkedNode *node2 = new LinkedNode(2);
+  LinkedNode *node3 = new LinkedNode(3);
+  LinkedNode *node4 = new LinkedNode(4);
+  LinkedNode *node5 = new LinkedNode(5);
+  LinkedNode *node6 = new LinkedNode(6);
 
-    display(node1);
-    auto head = reorder(node1);
-    display(head);
+  node1->next = node2;
+  node2->next = node3;
+  node3->next = node4;
+  node4->next = node5;
+  node5->next = node6;
+
+  display(node1);
+  auto head = reorder(node1);
+  display(head);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
+// Tips for Getting Started:
 //   1. Use the Solution Explorer window to add/manage files
 //   2. Use the Team Explorer window to connect to source control
 //   3. Use the Output window to see build output and other messages
 //   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+//   5. Go to Project > Add New Item to create new code files, or Project > Add
+//   Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project
+//   and select the .sln file
--- NeetCode/Reorder_list.h
+++ NeetCode/Reorder_list.h
@@ -4,71 +4,70 @@
 using namespace std;
 
 struct LinkedNode {
-    int val;
-    LinkedNode* next;
-    LinkedNode(int x) : val(x), next(nullptr) {}
+  int val;
+  LinkedNode *next;
+  LinkedNode(int x) : val(x), next(nullptr) {}
 };
 
-void display(LinkedNode* head) {
-    LinkedNode* curr = head;
-    cout << "List = ";
-    while (curr) {
-        cout << curr->val << " ";
-        curr = curr->next;
-    }
-    cout << endl;
+void display(LinkedNode *head) {
+  LinkedNode *curr = head;
+  cout << "List = ";
+  while (curr) {
+    cout << curr->val << " ";
+    curr = curr->next;
+  }
+  cout << endl;
 }
 
-LinkedNode* reverse(LinkedNode* head) {
-    LinkedNode* curr = head;
-    LinkedNode* prev = nullptr;
-    LinkedNode* temp = nullptr;
+LinkedNode *reverse(LinkedNode *head) {
+  LinkedNode *curr = head;
+  LinkedNode *prev = nullptr;
+  LinkedNode *temp = nullptr;
 
-    while (curr) {
-        temp = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = temp;
-    }
+  while (curr) {
+    temp = curr->next;
+    curr->next = prev;
+    prev = curr;
+    curr = temp;
+  }
 
-    head = prev;
-    
-    return head;
+  head = prev;
+  return head;
 }
 
-LinkedNode* reorder(LinkedNode* head) {
-    LinkedNode* f = head;
-    LinkedNode* s = head;
-    LinkedNode* f_temp = nullptr;
-    LinkedNode* s_temp = nullptr;
+LinkedNode *reorder(LinkedNode *head) {
+  LinkedNode *f = head;
+  LinkedNode *s = head;
+  LinkedNode *f_temp = nullptr;
+  LinkedNode *s_temp = nullptr;
 
-    while (f->next && f->next->next) {
-        s = s->next;
-        f = f->next->next;
-    }
+  while (f->next && f->next->next) {
+    s = s->next;
+    f = f->next->next;
+  }
 
+  s_temp = s->next;
+  s->next = nullptr;
+
+  if (f->next) {
+    f = f->next;
+  }
+
+  f_temp = reverse(s_temp);
+
+  f = f_temp;
+  s = head;
+
+  while (f) {
     s_temp = s->next;
-    s->next = nullptr;
+    f_temp = f->next;
 
-    if (f->next) {
-        f = f->next;
-    }
-    
-    f_temp = reverse(s_temp);
+    s->next = f;
+    f->next = s_temp;
 
+    s = s_temp;
     f = f_temp;
-    s = head;
+  }
 
-    while (f) {
-        s_temp = s->next;
-        f_temp = f->next;
-
-        s->next = f;
-        f->next = s_temp;
-
-        s = s_temp;
-        f = f_temp;
-    }
-
-    return head;
+  return head;
 }
