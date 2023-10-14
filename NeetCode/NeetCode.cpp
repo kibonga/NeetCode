6547a12e9fd763a2a8620eab4a21fea8a540fd86
kibonga <pavledjurdjic30@gmail.com>
2023-10-14 21:26:22 +0200

    Working solution for reversing bits in uint

--- NeetCode/NeetCode.cpp
+++ NeetCode/NeetCode.cpp
@@ -1,11 +1,15 @@
 // NeetCode.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <cstdint>
 #include <iostream>
+#include "Reverse_bits.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+  uint32_t n = 43261596;
+  uint32_t result = revers_bits(n);
+  cout << "Result = " << result << endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
---
+++ NeetCode/Reverse_bits.h
@@ -0,0 +1,15 @@
+#include <cstdint>
+#include <iostream>
+using namespace std;
+
+uint32_t revers_bits(uint32_t n) {
+  uint32_t result = 0;
+
+  for(int i=0; i < 32; i++) {
+    result <<= 1;
+    result |= n & 1;
+    n >>= 1;
+  }
+
+  return result;
+}
